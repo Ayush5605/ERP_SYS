4b4ad2c71e6e5e50651100217efc6ad5bd55cc1f
Ayush Auti <ayushauti5605@gmail.com>
2026-01-12 16:32:58 +0530

    dashboard design started

--- school-erp-frontend/src/pages/Dashboard/StudentDashboard.jsx
+++ school-erp-frontend/src/pages/Dashboard/StudentDashboard.jsx
@@ -1,3 +1,19 @@
-export default function StudentDashboard() {
-  return <h1 className="text-2xl font-bold">Student Dashboard</h1>;
+import NoticeCard from "../../layouts/cards/NoticeCard.jsx";
+import LectureCard from "../../layouts/cards/LectureCard.jsx";
+import SocialConnectCard from "../../layouts/cards/SocialConnectCard.jsx";
+import  ExamCard from "../../layouts/cards/ExamCard.jsx";
+
+
+export default function StudentDashboard({data}){
+  return(
+
+     <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+      <NoticeCard notices={data.notices} />
+      <LectureCard lectures={data.lectures} />
+      <SocialConnectCard people={data.social} />
+      <ExamCard exams={data.exams} />
+    </div>
+
+  )
+  
 }
