9c79f55820a0de49ac3f486dd798568dcf268b45
Ayush Auti <ayushauti5605@gmail.com>
2026-01-15 21:01:56 +0530

    all the routes connected for side-bar options

--- school-erp-frontend/src/App.jsx
+++ school-erp-frontend/src/App.jsx
@@ -1,17 +1,31 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
-import ProtectedRoute from "./components/ProtectedRoute";
 import DashboardLayout from "./layouts/DahboardLayout.jsx";
-import { ROLES } from "./constants/roles.js";
 
-import AdminDashboard from "./pages/Dashboard/AdminDashboard.jsx";
-import TeacherDashboard from "./pages/Dashboard/TeacherDashboard.jsx";
-import StudentDashboard from "./pages/Dashboard/StudentDashboard.jsx";
-import ParentDashboard from "./pages/Dashboard/ParentDashboard.jsx";
-import SuperAdminDashboard from "./pages/Dashboard/SuperAdminDashboard.jsx";
-import Dashboard from "./pages/Dashboard";
+import Dashboard from "./pages/Dashboard.jsx";
 
-import { UserProvider } from "./context/UserContext.jsx"; // ✅ ADD THIS
+/* Normal pages */
+import Students from "./pages/students/Students.jsx";
+import Academics from "./pages/academics/Academics";
+import Fees from "./pages/fees/Fees.jsx";
+import Attendance from "./pages/attendance/Attendance.jsx";
+import Examination from "./pages/examination/Examination";
+
+/* Super Admin pages */
+import Schools from "./pages/super-admin/Schools.jsx";
+import Subscriptions from "./pages/super-admin/Subscriptions.jsx";
+import Security from "./pages/super-admin/Security.jsx";
+
+import { UserProvider } from "./context/UserContext.jsx";
+import Homewrok from "./pages/homework/Homework";
+import Inventory from "./pages/inventory/Inventory";
+import Library from "./pages/library/Library";
+import Transport from "./pages/transport/Transport";
+import AuditLog from "./pages/super-admin/AuditLogs";
+import Settings from "./pages/super-admin/Settings";
+import Hostel from "./pages/hostel/Hostel";
+import Overview from "./pages/super-admin/Overview";
+import Admin from "./pages/super-admin/Admin";
 
 export default function App() {
   return (
@@ -20,11 +34,32 @@ export default function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
 
-          {/* DASHBOARD LAYOUT */}
-          <Route path="/dashboard" element={<DashboardLayout />}>
-            <Route index element={<Dashboard />} />
+          {/* MAIN LAYOUT */}
+          <Route element={<DashboardLayout />}>
+            {/* ROLE-BASED DASHBOARD */}
+            <Route path="/dashboard" element={<Dashboard />} />
 
-           
+            {/* SIDEBAR ROUTES */}
+            <Route path="/academics" element={<Academics/>}/>
+            <Route path="/students" element={<Students />} />
+            <Route path="/examination" element={<Examination/>}/>
+            <Route path="homework" element={<Homewrok/>}/>
+            <Route path="/hostel" element={<Hostel/>}/>
+            <Route path="/inventory" element={<Inventory/>}/>
+            <Route path="/library" elements={<Library/>}/>
+            <Route path="/transport" element={<Transport/>}/>
+
+            <Route path="/fees" element={<Fees />} />
+            <Route path="/attendance" element={<Attendance />} />
+
+            {/* SUPER ADMIN ROUTES */}
+            <Route path="/super-admin/schools" element={<Schools />} />
+            <Route path="/super-admin/subscriptions" element={<Subscriptions />} />
+            <Route path="/super-admin/security" element={<Security />} />
+            <Route path="/super-admin/auditlog" element={<AuditLog/>}/>
+            <Route path="/super-admin/setting" element={<Settings/>}/>
+            <Route path="/super-admin/sa-dashboard" element={<Overview/>}/>
+            <Route path="/super-admin/school-admins" element={<Admin/>}/>
           </Route>
         </Routes>
       </BrowserRouter>
